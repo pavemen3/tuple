6e4b90bf92b94aa971429508111d43f24340b4ab
Keisuke Matsuda <matsuda@wisesips.com>
2022-10-23 14:37:47 +0900

    slice_sum

---
+++ slice_sum.rs
@@ -0,0 +1,17 @@
+fn sum_slice(items: &[i64]) -> i64 {
+  let mut total = 0;
+  for i in items {
+    total += i;
+  }
+  total // 合計値を返す
+}
+
+fn main() {
+  // 配列を初期化
+  let a = [1,2,3,4,5,6,7,8,9,10];
+  println!("a={}", sum_slice(&a[..]));
+  // ベクターを初期化
+  let b = vec![1,2,3,4,5,6,7,8,9,10];
+  println!("b={}", sum_slice(&b[..]));
+}
+
